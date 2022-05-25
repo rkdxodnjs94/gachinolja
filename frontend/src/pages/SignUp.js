bed0e8930bd8c76de8e61956ee50ce8358ed045d
gangtaewon <rkdxodnjs94@naver.com>
2022-05-26 00:05:16 +0900

    5/25 새벽12시 회원가입 프론트 유효성검사완료

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,41 +1,105 @@
-import React, { useState } from 'react';
+import './SignUp.css';
+import React, { useCallback, useState } from 'react';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Form, Col, Button } from 'react-bootstrap';
 import axios from 'axios';
 
-
 function SignUp(){
-  const [userId, setUserId] = useState();
-  const [userPw, setUserPw] = useState();
-  const [pwCheck, setPwCheck] = useState();
-  const [pwError, setPwError] = useState(false);
-  const [nickname, setNickname] = useState();
-  const [gender, setGender] = useState();
-  const [phone, setPhone] = useState();
-
-  const onChange = (e) => {
-    if (e.target.name === 'userid'){
-      setUserId(e.target.value);
-    } else if (e.target.name === 'userpw'){
-      setUserPw(e.target.value);
-    } else if (e.target.name === 'pwcheck') {
-      setPwError(e.target.value !== userPw);
-      setPwCheck(e.target.value);
-    }else if (e.target.name === 'nickname'){
-      setNickname(e.target.value);
-    } else if (e.target.name === 'gender'){
+  // 아이디, 비밀번호, 닉네임, 성별, 전화번호
+  const [userId, setUserId] = useState('');
+  const [userPw, setUserPw] = useState('');
+  const [userPwCheck, setUserPwCheck] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [gender, setGender] = useState('');
+  const [phone, setPhone] = useState('');
+  // 오류메시지 상태저장
+  const [idError, setIdError] = useState('');
+  const [pwError, setPwError] = useState('');
+  const [pwCheckError, setPwCheckError] = useState('');
+  const [nickError, setNickError] = useState('');
+  const [telError, setTelError] = useState('');
+  // 유효성 검사
+  const [idCheck, setIdCheck] = useState(false);
+  const [pwCheck, setPwCheck] = useState(false);
+  const [pwConfirmCheck, setPwConfirmCheck] = useState(false);
+  const [nickCheck, setNickCheck] = useState(false);
+  const [telCheck, setTelCheck] = useState(false);
+  // ID
+  const onChangeId = useCallback((e) => {
+    const emailRegex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    const emailCurrent = e.target.value;
+    setUserId(emailCurrent);
+    if ( !emailRegex.test(emailCurrent) ){
+      setIdError('이메일 형식이 틀렸어요!ㅠㅠ');
+      setIdCheck(false);
+    } else if ((e.target.value.length > 30) || (e.target.value.length < 10)) {
+      setIdError('10자 이상 30자 이하로 입력해주세요.');
+      setIdCheck(false);
+    } else {
+      setIdError('올바른 이메일 형식입니다!');
+      setIdCheck(true);
+    }
+  },[])
+  // PW
+  const onChangePw = useCallback((e) => {
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{10,15}$/;
+    const passwordCurrent = e.target.value;
+    setUserPw(passwordCurrent);
+    if (!passwordRegex.test(passwordCurrent)){
+      setPwError('숫자+영문자+특수문자 조합으로 10~15자 입력해주세요');
+      setPwCheck(false);
+    } else {
+      setPwError('안전한 비밀번호에요 :)');
+      setPwCheck(true);
+    }
+  },[])
+  // PwCheck
+  const onChangePwConfirm = useCallback((e) => {
+    const pwConfirmCurrent = e.target.value;
+    setUserPwCheck(pwConfirmCurrent);
+    if (userPw === pwConfirmCurrent){
+      setPwCheckError('비밀번호를 똑같이 입력했어요 XD');
+      setPwConfirmCheck(true);
+    } else {
+      setPwCheckError('비밀번호가 일치하지 않아요 ㅠㅠ');
+      setPwConfirmCheck(false);
+    }
+  },[userPw])
+  // nickname
+  const onChangeNick = useCallback((e) => {
+    setNickname(e.target.value);
+    if (e.target.value.length > 12 || e.target.value.length < 3) {
+      setNickError('3글자 이상 12글자 이하로 입력해주세요.')
+      setNickCheck(false)
+    } else {
+      setNickError('올바른 닉네임 형식입니다 :)')
+      setNickCheck(true)
+    }
+  },[])
+  // 성별
+  const onChangeGen = (e) => {
+    if (e.target.name === 'gender'){
       setGender(e.target.value);
-    } else if (e.target.name === 'tel'){
-      setPhone(e.target.value);
     }
   }
+  // 전화번호
+  const onChangeTel = useCallback((e) => {
+    const TelRegex = /(\d{3}).*(\d{3}).*(\d{4})/;
+    const telCurrent = e.target.value;
+    setPhone(telCurrent);
+
+    if (!TelRegex.test(telCurrent)) {
+      setTelError('전화번호 형식이 틀렸어요! 다시 확인해주세요!!');
+      setTelCheck(false);
+    } else {
+      setTelError('올바른 전화번호 형식이에요 =)');
+      setTelCheck(true);
+    }
+  }, [])
 
   const onRegister = (e) => {
     e.stopPropagation();
-    if (userPw !== pwCheck) {
-      return setPwError(true);
-    }
     try { 
       const response = axios.post('/api/user/register',{
         userid : userId,
@@ -58,7 +122,8 @@ function SignUp(){
         <Form.Group className="mb-3" controlId="userid">
           <Form.Label>아이디</Form.Label>
           <Form.Control name='userid' type="email" placeholder="name@example.com" className="w-25"
-          onChange={onChange} required/>
+          onChange={onChangeId} required/>
+          {userId.length > 0 && <span className={`${idCheck ? 'success' : 'error'}`}>{idError}</span>}
         </Form.Group>
         <Form.Group className="mb-3" controlId="password">
           <Form.Label column sm="2">
@@ -66,7 +131,8 @@ function SignUp(){
           </Form.Label>
           <Col>
             <Form.Control name="userpw" type="password" placeholder="Password" className="w-25"
-            onChange={onChange} required/>
+            onChange={onChangePw} required/>
+            {userPw.length > 0 && <span className={`${pwCheck ? 'success' : 'error'}`}>{pwError}</span>}
           </Col>
         </Form.Group>
         <Form.Group className="mb-3" controlId="confirmpassword">
@@ -74,8 +140,9 @@ function SignUp(){
             비밀번호 확인
           </Form.Label>
           <Col>
-            <Form.Control name="pwcheck" type="password" placeholder="Password" className="w-25" required/>
-            {pwError && <div style={{color : 'red'}}>비밀번호가 일치하지 않습니다.</div>}
+            <Form.Control name="pwcheck" type="password" placeholder="Password" className="w-25"
+            required onChange={onChangePwConfirm}/>
+            {userPwCheck.length > 0 && <span className={`${pwConfirmCheck ? 'success' : 'error'}`}>{pwCheckError}</span>}
           </Col>
         </Form.Group>
         <Form.Group className="mb-3" controlId="nickname">
@@ -83,21 +150,25 @@ function SignUp(){
             닉네임
           </Form.Label>
           <Form.Control name="nickname" type="text" placeholder="3~12자" className="w-25"
-          onChange={onChange} required/>
+          onChange={onChangeNick} required/>
+          {nickname.length > 0 && <span className={`${nickCheck ? 'success' : 'error'}`}>{nickError}</span>}
         </Form.Group>
         <Form.Group className="mb-1" controlId="gender">
           <Form.Label column sm='1'>
             성별
           </Form.Label>
-          <Form.Check inline type='radio' name='gender' value='male' id='male' label='남자' onChange={onChange} required/>
-          <Form.Check inline type='radio' name='gender' value='female' id='female' label='여자' onChange={onChange}/>
+          <Form.Check inline type='radio' name='gender' value='male' id='male' label='남자' onChange={onChangeGen} required/>
+          <Form.Check inline type='radio' name='gender' value='female' id='female' label='여자' onChange={onChangeGen}/>
         </Form.Group>
         <Form.Group controlId="tel">
           <Form.Label column sm="2">
             전화번호
           </Form.Label>
           <Form.Control name='tel' type="text" placeholder="전화번호" className="w-25"
-          onChange={onChange} required/>
+          onChange={onChangeTel} required/>
+          {phone.length > 0 && (
+            <span className={`${telCheck ? 'success' : 'error'}`}>{telError}</span>
+          )}
         </Form.Group>
       </Form>
       <div style={{marginLeft:'20%', marginBottom: '3%'}}>
