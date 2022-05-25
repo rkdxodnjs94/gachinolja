0a042bab95bea0ca5da1e5ae80c222a767cd3d9f
gangtaewon <rkdxodnjs94@naver.com>
2022-05-25 18:16:55 +0900

    5/25 회원가입 post DB전달완료

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Form, Col, Button } from 'react-bootstrap';
@@ -5,20 +6,43 @@ import axios from 'axios';
 
 
 function SignUp(){
+  const [userId, setUserId] = useState();
+  const [userPw, setUserPw] = useState();
+  const [pwCheck, setPwCheck] = useState();
+  const [pwError, setPwError] = useState(false);
+  const [nickname, setNickname] = useState();
+  const [gender, setGender] = useState();
+  const [phone, setPhone] = useState();
 
   const onChange = (e) => {
-    return e.target.value;
-    // const { name, value } = e.target;
-    // console.log(name, value);
+    if (e.target.name === 'userid'){
+      setUserId(e.target.value);
+    } else if (e.target.name === 'userpw'){
+      setUserPw(e.target.value);
+    } else if (e.target.name === 'pwcheck') {
+      setPwError(e.target.value !== userPw);
+      setPwCheck(e.target.value);
+    }else if (e.target.name === 'nickname'){
+      setNickname(e.target.value);
+    } else if (e.target.name === 'gender'){
+      setGender(e.target.value);
+    } else if (e.target.name === 'tel'){
+      setPhone(e.target.value);
+    }
   }
-  async function Axios(e){
+
+  const onRegister = (e) => {
+    e.stopPropagation();
+    if (userPw !== pwCheck) {
+      return setPwError(true);
+    }
     try { 
       const response = axios.post('/api/user/register',{
-        userid : onChange(),
-        userpw : onChange(),
-        nickname : onChange(),
-        gender : onChange(),
-        tel : onChange()
+        userid : userId,
+        userpw : userPw,
+        nickname : nickname,
+        gender : gender,
+        tel : phone
       });
       console.log(response);
     } catch (error) {
@@ -30,11 +54,11 @@ function SignUp(){
     <>
       <Header />
       <div style={{height: '100px'}} className='container-fluid border'>회원가입</div>
-      <Form action='/api/user/register' className="container p-5">
+      <Form className="container px-5 py-4" style={{minWidth:'1000px'}}>
         <Form.Group className="mb-3" controlId="userid">
           <Form.Label>아이디</Form.Label>
           <Form.Control name='userid' type="email" placeholder="name@example.com" className="w-25"
-          onChange={onChange}/>
+          onChange={onChange} required/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="password">
           <Form.Label column sm="2">
@@ -42,7 +66,7 @@ function SignUp(){
           </Form.Label>
           <Col>
             <Form.Control name="userpw" type="password" placeholder="Password" className="w-25"
-            onChange={onChange}/>
+            onChange={onChange} required/>
           </Col>
         </Form.Group>
         <Form.Group className="mb-3" controlId="confirmpassword">
@@ -50,7 +74,8 @@ function SignUp(){
             비밀번호 확인
           </Form.Label>
           <Col>
-            <Form.Control name="userpw" type="password" placeholder="Password" className="w-25"/>
+            <Form.Control name="pwcheck" type="password" placeholder="Password" className="w-25" required/>
+            {pwError && <div style={{color : 'red'}}>비밀번호가 일치하지 않습니다.</div>}
           </Col>
         </Form.Group>
         <Form.Group className="mb-3" controlId="nickname">
@@ -58,24 +83,26 @@ function SignUp(){
             닉네임
           </Form.Label>
           <Form.Control name="nickname" type="text" placeholder="3~12자" className="w-25"
-          onChange={onChange}/>
+          onChange={onChange} required/>
         </Form.Group>
         <Form.Group className="mb-1" controlId="gender">
           <Form.Label column sm='1'>
             성별
           </Form.Label>
-          <Form.Check inline type='radio' name='gender' value='male' id='male' label='남자' onChange={onChange}/>
+          <Form.Check inline type='radio' name='gender' value='male' id='male' label='남자' onChange={onChange} required/>
           <Form.Check inline type='radio' name='gender' value='female' id='female' label='여자' onChange={onChange}/>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="tel">
+        <Form.Group controlId="tel">
           <Form.Label column sm="2">
             전화번호
           </Form.Label>
           <Form.Control name='tel' type="text" placeholder="전화번호" className="w-25"
-          onChange={onChange}/>
+          onChange={onChange} required/>
         </Form.Group>
-        <Button type='post' variant="outline-primary" onClick={Axios}>가입하기</Button>
       </Form>
+      <div style={{marginLeft:'20%', marginBottom: '3%'}}>
+        <Button type='post' variant="outline-primary" onClick={onRegister}>가입하기</Button>
+      </div>
       <Footer />
     </>
   )
